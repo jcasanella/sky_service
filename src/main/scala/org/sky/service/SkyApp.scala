34895d3a81e16cbd365f41ec145947fb619c52d1
jordi <jordikiter@hotmail.es>
2020-04-16 22:48:21 +0100

    Graceful termination and get props from application.conf

--- src/main/scala/org/sky/service/SkyApp.scala
+++ src/main/scala/org/sky/service/SkyApp.scala
@@ -4,9 +4,13 @@ import akka.actor.ActorSystem
 import akka.event.Logging
 import akka.stream.ActorMaterializer
 import akka.http.scaladsl.Http
+import com.typesafe.config.ConfigFactory
 
 import scala.io.StdIn
 import org.sky.service.routes.SkyRoutes
+
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
 
 object SkyApp extends App {
 
@@ -14,16 +18,23 @@ object SkyApp extends App {
   implicit val materializer = ActorMaterializer
   implicit val executionContext = system.dispatcher
 
+  val ip_port = {
+    val config = ConfigFactory.load()
+    (config.getString("sky-app.interface"), config.getInt("sky-app.port"))
+  }
+
   val log = Logging(system, "SpecialRoutes")
   val skyRoutes = new SkyRoutes(log)
-  val bindingFuture = Http().bindAndHandle(skyRoutes.routes, "localhost", 8080)
+  val bindingFuture = Http().bindAndHandle(skyRoutes.routes, ip_port._1, ip_port._2)
 
-  println(s"Server online at http://localhost:8080\nPress return to stop")
+  println(s"Server online at http://${ip_port._1}:${ip_port._2}\nPress return to stop")
   StdIn.readLine()
 
-  bindingFuture
-    .flatMap(_.unbind())
-    .onComplete(_ => system.terminate())
+  val onceAllConnectionsTerminated: Future[Http.HttpTerminated] =
+    Await.result(bindingFuture, 10.seconds)
+      .terminate(hardDeadline = 3.seconds)
+
+  onceAllConnectionsTerminated.flatMap( _ => system.terminate())
 }
 
 
